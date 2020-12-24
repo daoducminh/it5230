9021ae8e4c76afda302351be6527e0338bec01b1
Minh Dao <minh.dd189669@sis.hust.edu.vn>
2020-12-24 21:55:58 +0700

    Add dish_views for users

--- django_project/foods/urls.py
+++ django_project/foods/urls.py
@@ -5,14 +5,17 @@ from . import views, user_views, admin_views, dish_views
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', include([
-        path('profile/', user_views.ProfileView.as_view(), name='account_profile'),
         path('register/', user_views.RegisterView.as_view(), name='account_register'),
+
+    ])),
+    path('users/', include([
+        path('profile/', user_views.ProfileView.as_view(), name='account_profile'),
         path('dish/', include([
-            # path('',,name='user_all_dishes'),
+            path('', dish_views.UserAllDishView.as_view(), name='user_all_dishes'),
             path('<int:pk>/', include([
-                # path('',,name='user_dish_detail'),
-                path('update/', dish_views.UserUpdateDishView.as_view(), name='user_update_dish'),
-                path('delete/', dish_views.UserDeleteDishView.as_view(), name='user_delete_dish')
+                path('', dish_views.UserDishView.as_view(), name='user_dish_detail'),
+                path('update/', dish_views.UpdateDishView.as_view(), name='user_update_dish'),
+                path('delete/', dish_views.DeleteDishView.as_view(), name='user_delete_dish')
             ]))
         ]))
     ])),
@@ -22,11 +25,11 @@ urlpatterns = [
         path('', admin_views.Index.as_view(), name='admin_index'),
         path('user/', admin_views.UserManagement.as_view(), name='user_management'),
         path('dish/', include([
-            path('', dish_views.AdminAllDishView.as_view(), name='dish_management'),
+            path('', dish_views.AdminAllDishView.as_view(), name='admin_all_dishes'),
             path('<int:pk>/', include([
-                path('', dish_views.AdminDishView.as_view(), name='user_dish_detail'),
-                path('update/', dish_views.UserUpdateDishView.as_view(), name='admin_update_dish'),
-                path('delete/', dish_views.UserDeleteDishView.as_view(), name='admin_delete_dish')
+                path('', dish_views.AdminDishView.as_view(), name='admin_dish_detail'),
+                path('update/', dish_views.UpdateDishView.as_view(), name='admin_update_dish'),
+                path('delete/', dish_views.DeleteDishView.as_view(), name='admin_delete_dish')
             ]))
         ])),
     ])),
