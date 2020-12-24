10169a1ce5632d1651ff6ac8b0b1c2233033df5e
trungpq164256 <trungpq.sondra@gmail.com>
2020-12-24 10:42:54 +0700

    add all dishes view

--- django_project/foods/dish_views.py
+++ django_project/foods/dish_views.py
@@ -1,5 +1,5 @@
 from django.forms.models import model_to_dict
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views.generic import View
 
 from .models import Dish
@@ -8,22 +8,28 @@ from .views import UserRequiredView
 
 class AllDishView(UserRequiredView):
     def get(self, request):
-        return render(request, '')
+        dishes = Dish.objects.all()
+        context = {'dishes': dishes}
+        return render(request, 'foods/dishes.html', context)
+    def post(self, request):
+        dishes = Dish.objects.all()
+        context = {'dishes': dishes}
+        return render(request, 'foods/dishes.html', context)
 
 
 class DishView(UserRequiredView):
     def get(self, request, dish_id):
         dish = Dish.objects.get(pk=dish_id)
+        context = {'dish': dish}
         if dish.is_public or (request.user.is_authenticated and request.user.pk == dish.user_id):
-            return render(request, '', model_to_dict(dish))
+            return render(request, 'foods/dish.html', context)
         else:
-            # Not authorized or not public
-            pass
+            return HttpResponse("Not authorized or not public")
 
     def post(self, request, dish_id):
         dish = Dish.objects.get(pk=dish_id)
+        context = {'dish': dish}
         if dish.is_public or (request.user.is_authenticated and request.user.pk == dish.user_id):
-            return render(request, '', model_to_dict(dish))
+            return render(request, 'foods/dish.html', context)
         else:
-            # Not authorized or not public
-            pass
+            return HttpResponse("Not authorized or not public")
--- django_project/foods/urls.py
+++ django_project/foods/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from . import views, user_views, admin_views
+from . import views, user_views, admin_views, dish_views
 
 urlpatterns = [
     path('', views.index),
     path('accounts/profile/', user_views.ProfileView.as_view(), name='account_profile'),
     path('base/', admin_views.TestBase.as_view(), name='base'),
+    path('dishes/', dish_views.AllDishView.as_view(), name='all_dish_view'),
+    path('dishes/<int:dish_id>/', dish_views.DishView.as_view(), name='dish_view'),
 ]
