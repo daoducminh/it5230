ade1add1f25e88eabe84352ad7a4d72c2bfabc85
doannd1997 <duydoan0907@gmail.com>
2020-12-24 22:06:16 +0700

    add menu routes

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('foods.urls'))
+    path('', include('foods.urls')),
+    path('menu/', include('menu.urls'))
 ]
---
+++ django_project/menu/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import menu_index
+urlpatterns = {
+    path('', menu_index, name="menu_index")
+}
