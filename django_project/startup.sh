8b70075ed01df15ec12963b1cc7df3683dec484a
Minh Dao <minh.dd189669@sis.hust.edu.vn>
2021-01-02 16:22:43 +0700

    Update docker-compose with private registry

--- django_project/startup.sh
+++ django_project/startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo ">>>>> Migrating the database before starting the server"
+echo "\n>>>>> Migrating the database before starting the server\n"
 python manage.py makemigrations
 python manage.py migrate
 
-echo ">>>>> Load data"
+echo "\n>>>>> Load data\n"
 python manage.py loaddata data.json
 
-echo ">>>>> Update score"
+echo "\n>>>>> Update score\n"
 python manage.py update_score
 
-echo ">>>>> Run server"
+echo "\n>>>>> Run server\n"
 python manage.py runserver 0.0.0.0:8000
