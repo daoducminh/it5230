a12f6938feebb2433ee72e7ade783ae1e9f72c3e
Minh Dao <minh.dd189669@sis.hust.edu.vn>
2021-01-02 22:08:35 +0700

    Update seeding database

--- django_project/seeds.py
+++ django_project/seeds.py
@@ -4,16 +4,25 @@ from json import dump
 
 from faker import Faker
 
+ADMINS = 20
+USERS = 30
+DISHES = 200
+
+ADMIN_RANGE = (1, 1+ADMINS)
+USER_RANGE = (1+ADMINS, 1+ADMINS+USERS)
+ALL_USER_RANGE = (1, 1+ADMINS+USERS)
+DISH_RANGE = (1, 1+DISHES)
+
 MODEL = 'model'
 PK = 'pk'
 FIELDS = 'fields'
-USER_ID = 2
 AUTH_USER = 'auth.user'
 FOODS_USER = 'foods.user'
 FOODS_DISH = 'foods.dish'
 FOODS_RATING = 'foods.rating'
 HASHED_PASSWORD = 'pbkdf2_sha256$216000$E7eK2AnWdjH7$vA0IUnEP2MzszO+Ubxp00DSiXq2AbT6wBDMex3XU00I='
 DATE_JOINED = '2020-12-23T14:49:21Z'
+BIRTHDAY = '2000-01-01'
 
 fake = Faker()
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -30,16 +39,50 @@ pool = {
 def seed_user():
     auth_users = []
     foods_users = []
-    for i in range(1, 21):
+    for i in range(*ADMIN_RANGE):
         gender = fake.pybool()
         auth_admin = {
             'password': HASHED_PASSWORD,
             'last_login': None,
             'is_superuser': True,
-            'username': 'admin{}'.format(i),
+            'username': 'admin_{}'.format(i),
             'first_name': fake.first_name_male() if gender else fake.first_name_female(),
             'last_name': fake.last_name_male() if gender else fake.last_name_female(),
-            'email': 'johndoe@gmail.com',
+            'email': fake.email(),
+            'is_staff': True,
+            'is_active': True,
+            'date_joined': DATE_JOINED,
+            'groups': (),
+            'user_permissions': ()
+        }
+        foods_admin = {
+            'user': i,
+            'birthday': BIRTHDAY,
+            'height': fake.pyint(min_value=100, max_value=250),
+            'weight': fake.pyint(min_value=20, max_value=200),
+            'gender': gender,
+            'diet_factor': 1
+        }
+        auth_users.append({
+            MODEL: AUTH_USER,
+            PK: i,
+            FIELDS: auth_admin
+        })
+        foods_users.append({
+            MODEL: FOODS_USER,
+            PK: i,
+            FIELDS: foods_admin
+        })
+    for i in range(*USER_RANGE):
+        gender = fake.pybool()
+        auth_user = {
+            'password': HASHED_PASSWORD,
+            'last_login': None,
+            'is_superuser': True,
+            'username': 'user_{}'.format(i),
+            'first_name': fake.first_name_male() if gender else fake.first_name_female(),
+            'last_name': fake.last_name_male() if gender else fake.last_name_female(),
+            'email': fake.email(),
             'is_staff': True,
             'is_active': True,
             'date_joined': DATE_JOINED,
@@ -47,55 +90,74 @@ def seed_user():
             'user_permissions': ()
         }
         foods_user = {
-
+            'user': i,
+            'birthday': BIRTHDAY,
+            'height': fake.pyint(min_value=100, max_value=250),
+            'weight': fake.pyint(min_value=20, max_value=200),
+            'gender': gender,
+            'diet_factor': 1
         }
+        auth_users.append({
+            MODEL: AUTH_USER,
+            PK: i,
+            FIELDS: auth_user
+        })
+        foods_users.append({
+            MODEL: FOODS_USER,
+            PK: i,
+            FIELDS: foods_user
+        })
+    return auth_users + foods_users
 
 
 def seed_food_dish():
     rs = []
-    for i in range(1, 51):
-        ingredients = []
-        for j in range(5):
-            ingredients.append(f'ingredient {random.randint(0, 1000)}')
-        d = {
-            'user': USER_ID,
-            'dish_name': f'dish {i}',
-            'description': fake.text(50),
-            'calories': random.randint(10, 3000),
-            'is_public': fake.pybool(),
-            'ingredients': ','.join(ingredients),
-            'created_at': timestamp,
-            'updated_at': timestamp
-        }
-        rs.append({
-            MODEL: FOODS_DISH,
-            PK: i,
-            FIELDS: d
-        })
+    for u in range(*ALL_USER_RANGE):
+        for i in range(*DISH_RANGE):
+            ingredients = []
+            for _ in range(5):
+                ingredients.append(f'ingredient {random.randint(0, 1000)}')
+            d = {
+                'user': u,
+                'dish_name': f'dish {u}-{i}',
+                'description': fake.text(50),
+                'calories': random.randint(10, 3000),
+                'is_public': fake.pybool(),
+                'ingredients': ','.join(ingredients),
+                'created_at': timestamp,
+                'updated_at': timestamp
+            }
+            rs.append({
+                MODEL: FOODS_DISH,
+                PK: i,
+                FIELDS: d
+            })
     return rs
 
 
 def seed_food_rating():
     rs = []
-    for i in range(1, 51):
-        r = {
-            'user': USER_ID,
-            'dish': i,
-            'score': random.randint(1, 5),
-            'comment': fake.text(50),
-            'created_at': timestamp,
-            'updated_at': timestamp
-        }
-        rs.append({
-            MODEL: FOODS_RATING,
-            PK: i,
-            FIELDS: r
-        })
+    for _ in range(5):
+        for i in range(*DISH_RANGE):
+            r = {
+                'user': random.randrange(*USER_RANGE),
+                'dish': i,
+                'score': random.randint(1, 5),
+                'comment': fake.text(50),
+                'created_at': timestamp,
+                'updated_at': timestamp
+            }
+            rs.append({
+                MODEL: FOODS_RATING,
+                PK: i,
+                FIELDS: r
+            })
     return rs
 
 
 if __name__ == '__main__':
-    a = seed_food_dish()
+    a = seed_user()
+    a += seed_food_dish()
     a += seed_food_rating()
-    with open('foods.json', 'w') as file:
+    with open('data.json', 'w') as file:
         dump(a, file)
